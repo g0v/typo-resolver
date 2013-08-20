eaa0c01087608f3612e916a950ed3caeea7e378d
kewang <cpckewang@gmail.com>
2013-08-21 00:19:27 +0800

    proto complete

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var newText = prompt("input new text");
+var selection = window.getSelection().getRangeAt(0);
+var span = document.createElement("span");
+var node = document.createTextNode(newText);
 
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-  console.log("text: " + info.selectionText);
+span.appendChild(node);
 
-  var newText = prompt("請修改文字：「" + info.selectionText + "」");
-
-  alert(newText);
-}
-
-// Create one test item for each context type.
-var contexts = ["page","selection","link","editable","image","video",
-                "audio"];
-for (var i = 0; i < contexts.length; i++) {
-  var context = contexts[i];
-  var title = "Test '" + context + "' menu item";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                       "onclick": genericOnClick});
-  console.log("'" + context + "' item:" + id);
-}
+selection.insertNode(span);
