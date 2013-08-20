c09a5797373e03c869369b4ba9919a3ce0a70e77
kewang <cpckewang@gmail.com>
2013-08-20 22:40:57 +0800

    add some file

---
+++ script.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// A generic onclick callback function.
+function genericOnClick(info, tab) {
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+  console.log("text: " + info.selectionText);
+
+  var newText = prompt("請修改文字：「" + info.selectionText + "」");
+
+  alert(newText);
+}
+
+// Create one test item for each context type.
+var contexts = ["page","selection","link","editable","image","video",
+                "audio"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Test '" + context + "' menu item";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": genericOnClick});
+  console.log("'" + context + "' item:" + id);
+}
