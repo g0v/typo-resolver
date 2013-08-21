b4cae3dc1ba69dcc414279023a1cba6f07738ad6
kewang <cpckewang@gmail.com>
2013-08-21 19:38:44 +0800

    modify css

--- modify.js
+++ modify.js
@@ -1,13 +1,14 @@
 var newText = prompt("input new text");
 var sel = window.getSelection();
 var range = sel.getRangeAt(0);
-var style = "background-color: red; font-weight: bold";
+var style = "background-color: green; font-weight: bold; color: white";
 var span = $.parseHTML("<span style='" + style + "'>" + newText + "</span>");
 var selectionContents = range.extractContents();
 var old = document.createElement("span");
 
 //mark old text
 old.style.textDecoration = "line-through";
+old.style.backgroundColor = "red";
 old.appendChild(selectionContents);
 range.insertNode(old);
 
