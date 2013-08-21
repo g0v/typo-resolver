5da8b4db70c613f1ec2994969e538bf20cf44a91
kewang <cpckewang@gmail.com>
2013-08-21 18:30:19 +0800

    from popup to background

---
+++ background.js
@@ -0,0 +1,30 @@
+var mnuParent = chrome.contextMenus.create({
+  "title": "Typo Resolver",
+    "contexts": ["all"]
+});
+var mnuModify = chrome.contextMenus.create({
+  "parentId": mnuParent,
+    "title": "Modify",
+    "contexts": ["selection"],
+    "onclick": modifyClick
+});
+var mnuSend = chrome.contextMenus.create({
+  "parentId": mnuParent,
+    "title": "Send",
+    "contexts": ["all"],
+    "onclick": sendClick
+});
+
+function modifyClick(){
+  chrome.tabs.executeScript(null, {file: "jquery.js"}, function(){
+    chrome.tabs.executeScript({file: "modify.js"});
+  });
+}
+
+function sendClick(){
+  chrome.tabs.executeScript(null, {file: "jquery.js"}, function(){
+    chrome.tabs.executeScript(null, {file: "html2canvas.js"}, function(){
+      chrome.tabs.executeScript({file: "send.js"});
+    });
+  });
+}
--- modify.js
+++ modify.js
@@ -1,13 +1,22 @@
 var newText = prompt("input new text");
 var sel = window.getSelection();
 var range = sel.getRangeAt(0);
+/*
 var span = document.createElement("span");
 var node = document.createTextNode(newText);
+*/
+var $span = $("<span></span>");
 
-span.appendChild(node);
+$span.html(newText);
+$span.css("background-color", "red");
+$span.css("font-weight", "bold");
 
-span.style.backgroundColor = "red";
-span.style.fontWeight = "bold";
+alert($span.get());
+
+//span.appendChild(node);
+
+//span.style.backgroundColor = "red";
+//span.style.fontWeight = "bold";
 
 range.deleteContents();
-range.insertNode(span);
+range.insertNode($span.get());
