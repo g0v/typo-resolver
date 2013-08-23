03f1eba973492ef5f9500ba6bb17894e8bcc0637
kewang <cpckewang@gmail.com>
2013-08-23 23:43:01 +0800

    screenshot watermark

--- send.js
+++ send.js
@@ -13,6 +13,24 @@ var body = "Hello, your site has some typo. The attachment has already highlight
 
 html2canvas([document.body], {
   onrendered: function(canvas){
+    var ctx=canvas.getContext('2d');
+
+    ctx.globalAlpha=0.5;
+    // setup text for filling
+    ctx.font = "72px Comic Sans MS" ;
+    ctx.fillStyle = "red";
+    // get the metrics with font settings
+    var metrics = ctx.measureText("Typo Resolver");
+    var width = metrics.width;
+    // height is font size
+    var height = 72;
+    // change the origin coordinate to the middle of the context
+    ctx.translate(canvas.width/2, canvas.height/2);
+    // rotate the context (so it's rotated around its center)
+    ctx.rotate(-Math.atan(canvas.height/canvas.width));
+    // as the origin is now at the center, just need to center the text
+    ctx.fillText("Typo Resolver",-width/2,height/2);
+
     var img = canvas.toDataURL("image/png");
 
     window.open(img);
