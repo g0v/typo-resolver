7a3aa29b1d50931ed5ec23f9543d9e2a1f50631e
kewang <cpckewang@gmail.com>
2013-08-31 15:59:32 +0800

    refine background.js

--- background.js
+++ background.js
@@ -18,20 +18,18 @@ chrome.contextMenus.create({
 });
 
 function fixClick(){
-  chrome.tabs.executeScript(null, {file: "jquery.js"}, function(){
-    chrome.tabs.executeScript(null, {file: "Typo.js"}, function(){
+  chrome.tabs.executeScript({file: "jquery.js"}, function(){
+    chrome.tabs.executeScript({file: "Typo.js"}, function(){
       chrome.tabs.executeScript({file: "fix.js"});
     });
   });
 }
 
 function feedbackClick(){
-  chrome.tabs.executeScript(null, {file: "jquery.js"}, function(){
-    chrome.tabs.executeScript(null, {file: "html2canvas.js"}, function(){
-      chrome.tabs.executeScript(null, {file: "Typo.js"}, function(){
-        chrome.tabs.executeScript(null, {code: "var arrTypo = '" + arrTypo + "';"}, function(){
-          chrome.tabs.executeScript({file: "feedback.js"});
-        });
+  chrome.tabs.executeScript({file: "jquery.js"}, function(){
+    chrome.tabs.executeScript({file: "Typo.js"}, function(){
+      chrome.tabs.executeScript({code: "var arrTypo = '" + arrTypo + "';"}, function(){
+        chrome.tabs.executeScript({file: "feedback.js"});
       });
     });
   });
